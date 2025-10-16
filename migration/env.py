068b63c3299daede3bf7c46c2544618c7e7b31bc
KingsonThomas <8kingson@gmail.com>
2025-10-16 16:44:34 +0530

    fix: :bug: changing environment file
    changing envirnoment file for issue #4

--- migration/env.py
+++ migration/env.py
@@ -5,12 +5,16 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.db.database import 
+
+from app.core.migration_settings import SYNC_DB_URL
+from app.models.models import Users
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option(
+    "sqlalchemy.url", str(SYNC_DB_URL)
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -20,7 +24,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.db.database import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
