53daa1d8fe7e7b2c8d56837c94e33614234087bf
KingsonThomas <8kingson@gmail.com>
2025-10-16 11:43:42 +0530

    refactor: Changed code
    Removed unused part of the code

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from pathlib import Path
+from sqlalchemy.orm import declarative_base
 from pydantic_settings import BaseSettings,SettingsConfigDict
 from pydantic import AnyUrl
-class config:
-    env_file=str(Path(__file__).resolve().parent.parent/".env")
 class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file='.env',extra="ignore")
     ASYNC_DATABASE_URL:str
 settings=Settings()
+Base=declarative_base()
 engine = create_async_engine(settings.ASYNC_DATABASE_URL)
