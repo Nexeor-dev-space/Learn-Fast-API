e0791963701f0189f59fbd4fca2c749df5e0e422
KingsonThomas <8kingson@gmail.com>
2025-10-27 14:45:48 +0530

    feat: :art: Added router
    Added router to veriy username for issue #5

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models.models import Users
 from app.schemas.schemas import *
@@ -7,16 +8,15 @@ from app.db.database import get_async_db
 router=APIRouter()
 
 @router.post("/auth/register")
-async def validate_user(users:UserCreate,db: get_async_db):
-    result = await db.execute(select(Users).filter(Users.username == users.username))
+async def validate_user(users:UserCreate,db: AsyncSession = Depends(get_async_db)):
+    result = await db.execute(select(Users).where(Users.username == users.username))
     existing_user = result.scalar_one_or_none()
     if existing_user:
-        print("Username already exists")
-    new_user = Users(username=users.username)
+        raise HTTPException(status_code=400,detail="Username already exists")
+    new_user=Users(username=users.username,fullname=users.fullname)
     new_user.set_password(users.password)
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
-
     return {"message": "User registered successfully", "username": new_user.username}
         
