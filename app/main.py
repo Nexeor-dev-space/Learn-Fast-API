2d2de4c8462762805308bafdb62083942a34f102
Damini Yadav <daminiyadav23062001@gmail.com>
2025-11-08 18:03:52 +0530

    issue #4 daminiyadav23

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from app.db import config  # ensure this path matches your folder structure
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "FastAPI is running successfully!"}
+async def read_root():
+    return {"message": "Hello, FastAPI is running!!"}
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+<<<<<<< HEAD
 class UserCreate(BaseModel):
     username: str
     fullname: str
@@ -9,6 +10,17 @@ class UserRead(BaseModel):
     id: int
     username: str
     fullname: str
+=======
+class UserBase(BaseModel):
+    username: str
+    fullname: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserRead(UserBase):
+    id: int
+>>>>>>> main-daminiyadav23
 
     class Config:
         orm_mode = True
