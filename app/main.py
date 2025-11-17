245c8e44cae819573e8cb21bc82d1d5d085f08c3
Megha Muralidharan <megha710sw@gmail.com>
2025-11-17 19:19:33 +0530

    Fix main.py imports for Issue 6

--- app/main.py
+++ app/main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from app.routes import register,login,user_routes
-# Correct import
-# from app.routes.user_routes import router as user_router 
 
 app = FastAPI()
-
-app.include_router(register.router)
-app.include_router(login.router)
-app.include_router(user_routes.router)
 
 @app.get('/')
 def index():
     return {'Message':'Hello World!'}
-    
