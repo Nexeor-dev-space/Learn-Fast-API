b2d759289abd54353c1850798279553080333279
Megha Muralidharan <megha710sw@gmail.com>
2025-11-17 20:23:49 +0530

    Updated

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from app.routes import auth
+from app.routes.register import router as register_router
+from app.routes.login import router as login_router
+from app.routes.user_routes import router as user_router
 
 app = FastAPI()
 
-app.include_router(auth.router)
+app.include_router(register_router)
+app.include_router(login_router)
+app.include_router(user_router)
 
-@app.get('/')
+@app.get("/")
 def index():
-    return {'Message':'Hello World!'}
+    return {"Message": "Hello World!"}
 
