4bdaa653e30e6dcdfc26012c1013b345975b36d6
Megha Muralidharan <megha710sw@gmail.com>
2025-11-18 10:42:51 +0530

    Modified env.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,20 +5,23 @@ import os
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-# Add project root to path
 sys.path.insert(0, os.path.abspath("."))
 
-# Load environment variables
 from dotenv import load_dotenv
 load_dotenv()
 
 # Import settings & Base
-from app.core.settings import settings
+from app.core.settings import settings  
 from app.db.database import Base
-from app.models.user import User  # Import your models here
+from app.models.user import User  
 
 # Alembic config
 config = context.config
+
+
+config.set_main_option(
+    "sqlalchemy.url", str(settings.SYNC_DATABASE_URL) 
+)
 
 # Logging from alembic.ini
 if config.config_file_name is not None:
@@ -27,12 +30,20 @@ if config.config_file_name is not None:
 # Metadata for autogenerate
 target_metadata = Base.metadata
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
-    url = settings.SYNC_DATABASE_URL
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -45,11 +56,14 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
     connectable = engine_from_config(
-        {
-            'sqlalchemy.url': settings.SYNC_DATABASE_URL
-        },
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
