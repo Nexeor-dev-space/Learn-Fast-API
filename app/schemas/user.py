d07c75cd90246773b4663254daae1ea4b3a2da40
Ganesh SN <ganeshsn205@gmail.com>
2026-03-18 16:28:52 +0530

    feat: :sparkles: #4 Added User model, hashing utilities, and migration

---
+++ app/schemas/user.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    fullname: str
+    password: str
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    fullname: str
+
+    model_config = {"from_attributes": True}
