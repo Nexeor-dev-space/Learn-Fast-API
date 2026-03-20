af24a746cd4a1830ee32fd2eea5cd7ffd79da81c
Ganesh SN <ganeshsn205@gmail.com>
2026-03-20 13:05:50 +0530

    feat: :sparkles: #5 Added user registration endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"message": "FastAPI server is running"}
+from app.routes.auth import router as auth_router
+
+app.include_router(auth_router)
+
+# @app.get("/")
+# async def home():
+#     return {"message": "FastAPI server is running"}
