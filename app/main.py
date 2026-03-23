e27ea6f6040efa67e9d7dac1a81656a67b0782fa
Ganesh SN <ganeshsn205@gmail.com>
2026-03-23 19:00:27 +0530

    feat: :sparkles: #8 Added CRUD routes for tasks

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from app.routes.auth import router as auth_router
+from app.routes.tasks import router as tasks_router
 
 app.include_router(auth_router)
+app.include_router(tasks_router)
 
 # @app.get("/")
 # async def home():
