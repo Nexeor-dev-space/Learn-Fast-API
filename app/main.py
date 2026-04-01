003027df9218be5fdced0817f8b31648b0619ba1
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2026-04-01 14:50:16 +0530

    feat: :sparkles: issue #8 add CRUD endpoints for tasks with JWT auth

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.routes.auth import router as auth_router
+from app.routes.tasks import router as tasks_router
+
 app = FastAPI(title = "ToDo API")
 
 app.include_router(auth_router)
+app.include_router(tasks_router)
+
 
 @app.get("/")
 async def root():
